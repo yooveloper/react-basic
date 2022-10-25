71fee6e0547e29e40c783554017f037580cb7f63
yooveloper <yooveloper@gmail.com>
2022-10-25 17:16:36 +0900

    ✅  커스텀 훅 만들기

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppMentorsButton from './AppMentorsButton';
+import AppClass from './AppClass';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppMentorsButton />
+    <AppClass />
   </React.StrictMode>
 );
 
